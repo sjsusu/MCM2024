253d2b2492db26f44f361adfe700e3b133969076
Riya Goyal <goyalr@go.stockton.edu>
2024-02-03 18:05:24 -0500

    made changes

--- reading data.py
+++ reading data.py
@@ -6,6 +6,7 @@ csv_file_path = '/Users/riyagoyal/Downloads/2024_MCM-ICM_Problems/Wimbledon_feat
 # Create an empty list to store the 2D array
 data_2d_array = []
 count = 0
+returncount = 0
 count3 = 7284
 firstserve = 0
 secondserve =0
@@ -21,6 +22,19 @@ with open(csv_file_path, 'r') as csv_file:
         # Append the row as a list to the 2D array
         data_2d_array.append(row)
 
+#Model 1
+for i in range(1,7284):
+    if data_2d_array[i][13] == data_2d_array[i][15]:
+        count += 1
+    else:
+        returncount +=1
+serverate = count/7284
+returnrate = returncount/7284
+print("Serve Rate:", serverate)
+print("Return Rate:", returnrate)
+
+
+# Model 2
 # Iterate over the rows and perform the desired calculations
 for i in range(1, 7284):
 #    if data_2d_array[i][13] == data_2d_array[i][15]:
@@ -35,13 +49,10 @@ for i in range(1, 7284):
                 sscount += 1
         else:
             count3 -= 1
-
-# Calculate rate and average
-rate = count / 7284
+# Calculate average
 fsaverage = firstserve/fscount
 ssaverage = secondserve/sscount
-# Display the resulting rate and average
-print("Rate:", rate)
+# Display the resulting average
 print("First Serve Average:", fsaverage)
 print("Second Serve Average:", ssaverage)
 
@@ -75,5 +86,51 @@ print("Average for Serve 2:", count2nd/count_2)
 print("First Serve Win Speed Average", countWin1/win1count)
 print("Second Serve Win Speed Average", countWin2/win2count)
 
+def calculate_probability_of_winning_game(rate):
+    return (
+        rate**4 +
+        4 * rate**4 * (1 - rate) +
+        10 * rate**4 * (1 - rate)**2 +
+        20 * rate**5 * (1 - rate)**3 / (1 - 2 * rate + 2 * rate**2)
+    )
 
+serverresult = calculate_probability_of_winning_game(serverate)
+returnresult = calculate_probability_of_winning_game(returnrate)
+print(f"The probability of winning the game when serving is: {serverresult}")
+print(f"The probability of winning the game when returning is: {returnresult}")
 
+tiebreak = (serverate+returnrate)/2
+print(tiebreak)
+
+def calculate_probability_of_winning_tiebreaker(pt):
+    return (
+        pt**7 +
+        7 * pt**7 * (1 - pt) +
+        28 * pt**7 * (1 - pt)**2 +
+        84 * pt**7 * (1 - pt)**3 +
+        210 * pt**7 * (1 - pt)**4 +
+        462 * pt**7 * (1 - pt)**5 +
+        924 * pt**8 * (1 - pt)**6 / (1 - 2 * pt + 2 * pt**2)
+    )
+
+result = calculate_probability_of_winning_tiebreaker(tiebreak)
+print(f"The probability of winning the tiebreaker is: {result}")
+keepcountserve = 0
+keepcountreturn =0
+for i in range(1,7284):
+    if data_2d_array[i][4] != data_2d_array[i-1][4]:
+        store = int(data_2d_array[i][13])
+    if int(data_2d_array[i][19]) != 0:
+        if store == int(data_2d_array[i][19]):
+            keepcountserve += 1
+        else:
+            keepcountreturn += 1
+            
+setserve = keepcountserve/(keepcountserve + keepcountreturn)
+setreturn = keepcountreturn/(keepcountserve + keepcountreturn)
+print("Probability of winning a set when serving first:", setserve)
+print("Probability of winning a set when returning first:", setreturn )
+
+  
+        
+        
